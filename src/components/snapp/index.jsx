d39e71f53b8258dd0ca848a0bbc44e359e2437b6
Celo <celociousalltrinmf@gmail.com>
2023-08-12 11:02:48 +0530

    Add Snap box responsive added

--- src/components/snapp/index.jsx
+++ src/components/snapp/index.jsx
@@ -7,23 +7,23 @@ import AppTextArea from "../app-text-area";
 
 const Snapp = () => {
   return (
-    <div className="d-flex row container pb-2 mb-4 ms-1 pt-2 border border-1 rounded">
-      <div className="col-md-1">
+    <div className="d-flex row border border-1 rounded pb-2 ms-1 me-1 mb-4 pt-2">
+      <div className="col-2 col-md-1">
         <img
           src={snappImage}
           alt="snappImage"
-          width="40px"
-          height="40px"
-          className="snapp-profile-image "
+          width="40rem"
+          height="40rem"
+          className="snapp-profile-image"
         />
       </div>
-      <div className="col-md-11">
-        <AppTextArea placeholder="Share your Snapps" />
+      <div className="col-10">
+        <AppTextArea placeholder="Share your Snapps" rows={1} />
         <div className="d-flex mt-1">
           <FiImage
             size={20}
             onClick={() => alert("Image upload button is clkicked")}
-            className="cursor-pointer"
+            className="cursor-pointer align-self-center"
           />
           <button
             className="btn btn-primary rounded-pill ms-auto"
