9fee53b2144ac618368223ca19353a28ebab4b30
Celo <celociousalltrinmf@gmail.com>
2023-08-13 12:55:37 +0530

    Add Friend responsive added

--- src/components/Friends/friends-list/index.jsx
+++ src/components/Friends/friends-list/index.jsx
@@ -16,7 +16,7 @@ const FriendsList = ({ MockFriendsList, isMessage, isSignup }) => {
           <div
             className={`d-flex row ${
               isSignup
-                ? "signup-friends-list-container shadow-sm mb-4 rounded"
+                ? "signup-friends-list-container ms-0 shadow-sm mb-4 rounded"
                 : "friends-list-container"
             }`}
             key={obj.id}
@@ -27,7 +27,11 @@ const FriendsList = ({ MockFriendsList, isMessage, isSignup }) => {
                 alt="profile_image"
                 width="50px"
                 height="50px"
-                className="friends_profile--img "
+                className={`${
+                  isSignup
+                    ? "signup-friends_profile--img mt-2 mt-md-3"
+                    : "friends_profile--img"
+                }`}
                 style={
                   isMessage && obj.connected
                     ? { border: "3px solid green" }
@@ -35,9 +39,15 @@ const FriendsList = ({ MockFriendsList, isMessage, isSignup }) => {
                 }
               />
             </div>
-            <div className="col-10 ms-4 ps-4 ps-md-0 ps-lg-3">
+            <div
+              className={`${
+                isSignup
+                  ? "col-10 ms-md-4 ms-lg-2 ps-lg-3"
+                  : "col-10 ms-4 ps-4 ps-md-0 ps-lg-3"
+              }`}
+            >
               <div className="d-flex justify-content-between mt-2">
-                <div>
+                <div className={`${isSignup && "ms-4 ps-2 ms-md-0 ps-md-0"}`}>
                   <p className="fw-bold mb-0">{obj.name}</p>
                   <p className="text-muted mb-0">
                     {" "}
@@ -45,12 +55,34 @@ const FriendsList = ({ MockFriendsList, isMessage, isSignup }) => {
                   </p>
                 </div>
                 <div className="d-flex align-items-center">
-                  <button className="btn btn-sm btn-primary">
-                    {isMessage ? "Send Message" : "Add friend"}
+                  <button
+                    className={`${
+                      isSignup && "text-nowrap"
+                    } btn btn-sm btn-primary`}
+                  >
+                    <span className={`${isSignup && "d-none d-md-block"}`}>
+                      {" "}
+                      {isMessage ? "Send Message" : "Add Friend"}{" "}
+                    </span>
+
+                    <span
+                      className={`${
+                        isSignup && "signup-addfriend-button d-md-none"
+                      }`}
+                    >
+                      {" "}
+                      {isMessage ? "Send Message" : "Add"}{" "}
+                    </span>
                   </button>
                 </div>
               </div>
-              <p>{obj.bio}</p>
+              <p
+                className={`${
+                  isSignup && "mt-1 ms-4 ps-2 ps-md-0 ms-md-0 mt-md-0"
+                }`}
+              >
+                {obj.bio}
+              </p>
             </div>
           </div>
         ))}
