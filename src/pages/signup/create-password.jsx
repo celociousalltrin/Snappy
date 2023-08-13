c08eb636fa1643a3e29ce1331cda03f33c340f3c
Celo <celociousalltrinmf@gmail.com>
2023-08-13 11:19:09 +0530

    create password responsive added

--- src/pages/signup/create-password.jsx
+++ src/pages/signup/create-password.jsx
@@ -4,13 +4,13 @@ import AppPasswordInput from "../../components/app-password-input";
 
 const CreatePassword = () => {
   return (
-    <div className="signup-create-password-container pt-1 pb-0">
-      <div>
-        <label>Create Password</label>
+    <div className="pt-1 pb-0 row d-flex justify-content-center ps-2 ps-md-0">
+      <div className="col-12 col-md-10 text-start">
+        <label className="ms-1">Create Password</label>
         <AppPasswordInput type="password" name="create_password" />
       </div>
-      <div className="mt-4">
-        <label>Confirm Password</label>
+      <div className="mt-4 col-12 col-md-10 text-start">
+        <label className="ms-1">Confirm Password</label>
         <AppPasswordInput type="password" name="Confirm_password" />
       </div>
     </div>
