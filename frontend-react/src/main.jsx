7da96a64c164411009b9434d9dd1684b06f65c8e
Celo <celociousalltrinmf@gmail.com>
2023-09-10 10:06:59 +0530

    redux persist implemented

--- frontend-react/src/main.jsx
+++ frontend-react/src/main.jsx
@@ -4,13 +4,19 @@ import App from "./App.jsx";
 import "./index.css";
 import { store } from "./redux/store.jsx";
 import { Provider } from "react-redux";
+import { PersistGate } from "redux-persist/integration/react";
+import { persistStore } from "redux-persist";
 
 // Bootstrap Package
 import "bootstrap/dist/css/bootstrap.min.css";
 import "bootstrap/dist/js/bootstrap.bundle.min.js";
 
+let persistor = persistStore(store);
+
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
-    <App />
+    <PersistGate loading={null} persistor={persistor}>
+      <App />
+    </PersistGate>
   </Provider>
 );
--- frontend-react/src/redux/store.jsx
+++ frontend-react/src/redux/store.jsx
@@ -1,8 +1,17 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 import userReducer from "./slices/userSlice";
+
+const persistConfig = {
+  key: "snappy_root",
+  storage,
+};
+
+const persistedReducer = persistReducer(persistConfig, userReducer);
 
 export const store = configureStore({
   reducer: {
-    user: userReducer,
+    user: persistedReducer,
   },
 });
