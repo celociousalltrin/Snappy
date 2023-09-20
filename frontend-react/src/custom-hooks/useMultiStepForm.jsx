7a77fefce9fa39db646ff66476a5d7f27e7ecbba
Celo <celociousalltrinmf@gmail.com>
2023-09-20 15:01:11 +0530

    custom hooks multi step form validation added

--- frontend-react/src/custom-hooks/useMultiStepForm.jsx
+++ frontend-react/src/custom-hooks/useMultiStepForm.jsx
@@ -1,11 +1,48 @@
 import React, { useState } from "react";
+import toast from "react-hot-toast";
 
-const useMultiStepForm = ({ props: { children } }) => {
+const useMultiStepForm = ({ props: { children } }, errors, touched) => {
   const [currentIndex, setCurrentIndex] = useState(0);
   const [direction, setDirection] = useState("next");
+
+  const dataValidate = (data) => {
+    return Object.keys(data).every((key) => {
+      if (Object.keys(data[key]).includes("0")) {
+        return true;
+      } else if (
+        Object.keys(data[key]).every(
+          (o) => Object.keys(data[key][o]).length > 0
+        )
+      ) {
+        return true;
+      } else {
+        return false;
+      }
+    });
+  };
+
+  const multiStepFormValidate = ({ data, isValid }) => {
+    if (isValid) {
+      return true;
+    } else {
+      if (data === undefined) {
+        return false;
+      } else {
+        return dataValidate(data);
+      }
+    }
+  };
+
   const next = () => {
-    setCurrentIndex(currentIndex + 1);
-    setDirection("next");
+    if (
+      !Object.keys(touched).filter((obj) => errors[obj] !== undefined).length &&
+      multiStepFormValidate(children[currentIndex].props)
+    ) {
+      setCurrentIndex(currentIndex + 1);
+      setDirection("next");
+    } else {
+      toast.error("Please Fill * all the mandatory fields");
+    }
   };
 
   const previous = () => {
