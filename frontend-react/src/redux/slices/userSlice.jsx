f289916a3f05dcac05e428ddda1e46e4fef88516
Celo <celociousalltrinmf@gmail.com>
2023-09-29 20:44:46 +0530

    jwt in slice

--- frontend-react/src/redux/slices/userSlice.jsx
+++ frontend-react/src/redux/slices/userSlice.jsx
@@ -1,13 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
+  data: {},
   count: 0,
 };
 
 export const userSlice = createSlice({
-  name: "counter",
+  name: "user",
   initialState,
   reducers: {
+    userData: (state, { payload }) => {
+      state.data = payload;
+    },
+    addNewAccesstoken: (state, { payload }) => {
+      state.data.access_token = payload;
+    },
+    logoutReducer: (state, { payload }) => {
+      state.data = initialState;
+    },
     increament: (state, { payload: { number } }) => {
       state.count += number;
     },
@@ -17,6 +27,12 @@ export const userSlice = createSlice({
   },
 });
 
-export const { increament, decrement } = userSlice.actions;
+export const {
+  increament,
+  decrement,
+  userData,
+  logoutReducer,
+  addNewAccesstoken,
+} = userSlice.actions;
 
 export default userSlice.reducer;
