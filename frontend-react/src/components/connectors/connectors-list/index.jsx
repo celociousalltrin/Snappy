64724ddbcac64c030cdde9611f4d5a7ecc4d7f8d
Celo <celociousalltrinmf@gmail.com>
2023-11-28 19:10:51 +0530

    bug fix

--- frontend-react/src/components/connectors/connectors-list/index.jsx
+++ frontend-react/src/components/connectors/connectors-list/index.jsx
@@ -101,36 +101,38 @@ const ConnectorsList = ({
             </div>
           )} */}
               <div>
-                {list.map((obj, index) => (
-                  <div
-                    className={`d-flex row ${
-                      isSignup
-                        ? "signup-connectors-list-container ms-0 shadow-sm mb-4 rounded"
-                        : "connectors-list-container"
-                    }`}
-                    key={obj._id}
-                  >
-                    <div className="col-1">
-                      <img
-                        src={obj.user_image?.secure_url}
-                        alt="profile_image"
-                        width="50px"
-                        height="50px"
-                        className={`${
-                          isSignup
-                            ? "signup-connectors_profile--img mt-2 mt-md-3"
-                            : "connectors_profile--img"
-                        }`}
-                        style={
-                          isMessage && obj.connected
-                            ? { border: "3px solid green" }
-                            : null
-                        }
-                      />
-                    </div>
-
+                {list
+                  .filter(({ user_name }) => !!user_name)
+                  .map((obj, index) => (
                     <div
-                      className={`
+                      className={`d-flex row ${
+                        isSignup
+                          ? "signup-connectors-list-container ms-0 shadow-sm mb-4 rounded"
+                          : "connectors-list-container"
+                      }`}
+                      key={obj._id}
+                    >
+                      <div className="col-1">
+                        <img
+                          src={obj.user_image?.secure_url}
+                          alt="profile_image"
+                          width="50px"
+                          height="50px"
+                          className={`${
+                            isSignup
+                              ? "signup-connectors_profile--img mt-2 mt-md-3"
+                              : "connectors_profile--img"
+                          }`}
+                          style={
+                            isMessage && obj.connected
+                              ? { border: "3px solid green" }
+                              : null
+                          }
+                        />
+                      </div>
+
+                      <div
+                        className={`
                ${
                  isSignup
                    ? "col-10 ms-md-4 ms-lg-2"
@@ -138,132 +140,132 @@ const ConnectorsList = ({
                    ? "col-9 ms-5"
                    : "col-10 ms-4 ps-4 ps-md-0 ps-lg-3"
                }`}
-                    >
-                      <div className="d-flex justify-content-between mt-2">
-                        <div
-                          className={`${
-                            isSignup && "ms-4 ps-2 ms-md-0 ps-md-0"
-                          }`}
-                        >
-                          {/* <AppPopover
+                      >
+                        <div className="d-flex justify-content-between mt-2">
+                          <div
+                            className={`${
+                              isSignup && "ms-4 ps-2 ms-md-0 ps-md-0"
+                            }`}
+                          >
+                            {/* <AppPopover
                     type={1}
                     isNoPopOver={isSignup || isDiscoverAlliance}
                   > */}
-                          <p
-                            className={`fw-bold mb-0 ${
-                              !isSignup ? "user-name-style" : ""
-                            }`}
-                            onClick={(e) =>
-                              !isSignup
-                                ? navigateToProfile(
-                                    e,
-                                    navigate,
-                                    obj._id,
-                                    page_id
-                                  )
-                                : null
-                            }
-                          >
-                            {`${obj.first_name} ${obj.last_name}`}
-                          </p>
-                          {/* </AppPopover> */}
-                          <p className="text-muted mb-0">
-                            {displayUserName(obj.user_name)}
-                          </p>
-                        </div>
-                        <div className="d-flex align-items-center">
-                          {!sec_id && (
-                            <AppFramerButton>
-                              <button
-                                className={`${isSignup && "text-nowrap"} ${
-                                  signupConnectedUsers?.includes(obj._id) ||
-                                  connectorsIDs.includes(obj._id) ||
-                                  isAllianceList
-                                    ? "btn btn-sm btn-danger"
-                                    : "btn btn-sm btn-primary"
-                                }`}
-                                onClick={() => {
-                                  if (isSignup) {
-                                    callback(obj._id);
-                                  } else {
-                                    if (
-                                      connectorsIDs.includes(obj._id) ||
-                                      isAllianceList
-                                    ) {
-                                      handleRemoveConnector(obj._id);
-                                    } else {
-                                      handleCreateConnector(obj._id);
-                                    }
-                                  }
-                                }}
-                              >
-                                {signupConnectedUsers?.includes(obj._id) ||
-                                connectorsIDs.includes(obj._id) ||
-                                isAllianceList ? (
-                                  <span
-                                    className={`${
-                                      isSignup && "d-none d-md-block"
-                                    }`}
-                                  >
-                                    {isMessage
-                                      ? "Send Message"
-                                      : "Remove Alliance"}{" "}
-                                  </span>
-                                ) : (
-                                  <span
-                                    className={`${
-                                      isSignup && "d-none d-md-block"
-                                    }`}
-                                  >
-                                    {" "}
-                                    {isMessage
-                                      ? "Send Message"
-                                      : "Add Alliance"}{" "}
-                                  </span>
-                                )}
-
-                                <span
-                                  className={`${
-                                    isSignup &&
-                                    "signup-addAlliance-button d-md-none"
-                                  } ${
-                                    (isMessage ||
-                                      isAllianceList ||
-                                      isFansList ||
-                                      isDiscoverAlliance ||
-                                      isConnectorList) &&
-                                    "d-none"
+                            <p
+                              className={`fw-bold mb-0 ${
+                                !isSignup ? "user-name-style" : ""
+                              }`}
+                              onClick={(e) =>
+                                !isSignup
+                                  ? navigateToProfile(
+                                      e,
+                                      navigate,
+                                      obj._id,
+                                      page_id
+                                    )
+                                  : null
+                              }
+                            >
+                              {`${obj.first_name} ${obj.last_name}`}
+                            </p>
+                            {/* </AppPopover> */}
+                            <p className="text-muted mb-0">
+                              {displayUserName(obj.user_name)}
+                            </p>
+                          </div>
+                          <div className="d-flex align-items-center">
+                            {!sec_id && (
+                              <AppFramerButton>
+                                <button
+                                  className={`${isSignup && "text-nowrap"} ${
+                                    signupConnectedUsers?.includes(obj._id) ||
+                                    connectorsIDs.includes(obj._id) ||
+                                    isAllianceList
+                                      ? "btn btn-sm btn-danger"
+                                      : "btn btn-sm btn-primary"
                                   }`}
+                                  onClick={() => {
+                                    if (isSignup) {
+                                      callback(obj._id);
+                                    } else {
+                                      if (
+                                        connectorsIDs.includes(obj._id) ||
+                                        isAllianceList
+                                      ) {
+                                        handleRemoveConnector(obj._id);
+                                      } else {
+                                        handleCreateConnector(obj._id);
+                                      }
+                                    }
+                                  }}
                                 >
-                                  Add
-                                </span>
-                              </button>
-                            </AppFramerButton>
-                          )}
+                                  {signupConnectedUsers?.includes(obj._id) ||
+                                  connectorsIDs.includes(obj._id) ||
+                                  isAllianceList ? (
+                                    <span
+                                      className={`${
+                                        isSignup && "d-none d-md-block"
+                                      }`}
+                                    >
+                                      {isMessage
+                                        ? "Send Message"
+                                        : "Remove Alliance"}{" "}
+                                    </span>
+                                  ) : (
+                                    <span
+                                      className={`${
+                                        isSignup && "d-none d-md-block"
+                                      }`}
+                                    >
+                                      {" "}
+                                      {isMessage
+                                        ? "Send Message"
+                                        : "Add Alliance"}{" "}
+                                    </span>
+                                  )}
+
+                                  <span
+                                    className={`${
+                                      isSignup &&
+                                      "signup-addAlliance-button d-md-none"
+                                    } ${
+                                      (isMessage ||
+                                        isAllianceList ||
+                                        isFansList ||
+                                        isDiscoverAlliance ||
+                                        isConnectorList) &&
+                                      "d-none"
+                                    }`}
+                                  >
+                                    Add
+                                  </span>
+                                </button>
+                              </AppFramerButton>
+                            )}
+                          </div>
                         </div>
+                        {!isDiscoverAlliance && (
+                          <div>
+                            <p
+                              className={`mb-2 ${
+                                isSignup &&
+                                "ms-4 mt-1 ps-1 ms-md-0 mt-md-0 p-md-0"
+                              }`}
+                            >
+                              <AppListExpand
+                                content={obj.about}
+                                contentId={index}
+                                isExpand={listUniqueId.includes(index)}
+                                showMore={showMore}
+                                showLess={showLess}
+                                isSignup={isSignup}
+                              />
+                            </p>
+                          </div>
+                        )}
                       </div>
-                      {!isDiscoverAlliance && (
-                        <div>
-                          <p
-                            className={`mb-2 ${
-                              isSignup &&
-                              "ms-4 mt-1 ps-1 ms-md-0 mt-md-0 p-md-0"
-                            }`}
-                          >
-                            <AppListExpand
-                              content={obj.about}
-                              contentId={index}
-                              isExpand={listUniqueId.includes(index)}
-                              showMore={showMore}
-                              showLess={showLess}
-                              isSignup={isSignup}
-                            />
-                          </p>
-                        </div>
-                      )}
                     </div>
-                  </div>
-                ))}
+                  ))}
               </div>
             </div>
           ) : (
